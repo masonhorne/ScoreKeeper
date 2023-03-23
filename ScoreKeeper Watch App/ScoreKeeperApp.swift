938d93ee820237795848c84a0deb7c0a0be87bcc
masonhorne <masonhorne1@gmail.com>
2023-03-23 15:45:39 -0400

    Initial Commit

---
+++ ScoreKeeper Watch App/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper Watch App
+//
+//  Created by Mason Horne on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
